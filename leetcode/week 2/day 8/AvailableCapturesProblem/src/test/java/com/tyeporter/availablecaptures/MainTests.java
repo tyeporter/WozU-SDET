46ec054757f522b90bf7e013ecd2b3ccce77b135
Tye Porter <tyeporter@outlook.com>
2020-10-14 22:11:07 -0500

    adding AvailableCapturesProblem (LeetCode #999) to repo, updating MatrixCellsProblem Main.java

---
+++ leetcode/week 2/day 8/AvailableCapturesProblem/src/test/java/com/tyeporter/availablecaptures/MainTests.java
@@ -0,0 +1,13 @@
+package com.tyeporter.availablecaptures;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MainTests {
+    
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+    
+}
--- leetcode/week 2/day 8/MatrixCellsProblem/src/main/java/com/tyeporter/matrixcellsproblem/Main.java
+++ leetcode/week 2/day 8/MatrixCellsProblem/src/main/java/com/tyeporter/matrixcellsproblem/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // Declare and instantiate Solution object
         Solution solution = new Solution();
 
-        // Test oddCells method
+        // Print number of odd cells in matrix
         int oddNumbersInMatrix = solution.oddCells(2, 3, new int[][]{{0, 1}, {1, 1}});
         System.out.println(oddNumbersInMatrix);
     }
